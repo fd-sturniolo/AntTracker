a9f8dfcaa577668b596543a30665ee5d2c4f7c1c
fd-sturniolo <74807992+fd-sturniolo@users.noreply.github.com>
2021-06-08 12:42:52 -0300

    Actualizar tracker/main.py

--- ant_tracker/tracker/main.py
+++ ant_tracker/tracker/main.py
@@ -1,10 +1,18 @@
+import os, sys
+from pathlib import Path
+ROOT_DIR = str(Path(__file__).parent.parent.parent)
+print(ROOT_DIR)
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
 import argparse
 import pathlib
 import pims
 
-from .parameters import DohSegmenterParameters, LogWSegmenterParameters, TrackerParameters
-from .segmenter import DohSegmenter, LogWSegmenter, Segmenter
-from .tracking import Tracker
+from ant_tracker.tracker.parameters import DohSegmenterParameters, LogWSegmenterParameters, TrackerParameters
+from ant_tracker.tracker.segmenter import DohSegmenter, LogWSegmenter, Segmenter
+from ant_tracker.tracker.tracking import Tracker
+from ant_tracker.tracker.common import ProgressBar
 
 def main(file: pathlib.Path, resolution, segmentver, play, outdir="./data"):
     seg_params = {
@@ -33,22 +41,29 @@ def main(file: pathlib.Path, resolution, segmentver, play, outdir="./data"):
     if play:
         tracker.track_viz(video, step_by_step=False, fps=60)
     else:
-        tracker.track_viz()
+        pbar = ProgressBar(len(video))
+        for frame in tracker.track_progressive():
+            pbar.next()
 
     outfile = pathlib.Path(outdir, f"{file.stem}-{tracker.version}.trk")
 
     tracker.info().save(outfile)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Generar .trk a partir de un video")
-    parser.add_argument('file')
-    parser.add_argument('--resolution', '-r', type=str, choices=['low', 'med', 'high'], help="Resolución del video",
-                        required=True)
-    parser.add_argument('--segmentver', '-s', type=int, choices=[1, 2], help="Versión del segmentador",
-                        required=True)
-    parser.add_argument('--play', '-p', type=bool, default=False)
-    parser.add_argument('--outputDir', '-o', type=str, default='./data',
-                        help="Directorio de salida")
-    args = parser.parse_args()
+    main(
+        pathlib.Path(r"E:\f\AntTracker\ant_tracker\tracker\vid_tags\New folder\HD1.mp4"), 'high', 1, False,
+        pathlib.Path(r"E:\f\AntTracker\ant_tracker\tracker\data")
+    )
 
-    main(pathlib.Path(args.file), args.resolution, args.segmentver, args.play, pathlib.Path(args.outputDir))
+    # parser = argparse.ArgumentParser(description="Generar .trk a partir de un video")
+    # parser.add_argument('file')
+    # parser.add_argument('--resolution', '-r', type=str, choices=['low', 'med', 'high'], help="Resolución del video",
+    #                     required=True)
+    # parser.add_argument('--segmentver', '-s', type=int, choices=[1, 2], help="Versión del segmentador",
+    #                     required=True)
+    # parser.add_argument('--play', '-p', type=bool, default=False)
+    # parser.add_argument('--outputDir', '-o', type=str, default='./data',
+    #                     help="Directorio de salida")
+    # args = parser.parse_args()
+
+    # main(pathlib.Path(args.file), args.resolution, args.segmentver, args.play, pathlib.Path(args.outputDir))
